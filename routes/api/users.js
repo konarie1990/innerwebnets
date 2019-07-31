e5025e6d0ff14d42f4ce7baa738efa9ac98c7144
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-07-31 13:00:24 -0500

    added gravatar

---
+++ routes/api/users.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+// Validate that a user needs to enter Name, Email and Password:
+// See docs for more info on express validator: https://express-validator.github.io/docs/
+const { check, validationResult } = require('express-validator');
+
+// bring in the user model
+const User = require('../../models/User');
+
+// @route       GET api/users
+// @description Register User
+// @access      Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'You must include a valid email').isEmail(),
+    check(
+      'password',
+      'You must enter a password with 6 or more characters'
+    ).isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body);
+
+    // Destructure the req.body so we don't have to keep using that
+    const { name, email, password } = req.body;
+
+    try {
+      // Check to see if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Grab the user's gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt the password using bcrypt
+      // Save the user to the db (use await because it's returning a promise)
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      // Return the jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect the Database
+connectDB();
+
+// Initialize Middleware - no need for body parser with new version of express
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define the Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
