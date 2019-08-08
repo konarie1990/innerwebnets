0fcd9b853d804ca0aec241bbc6cf00c55eaed18a
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-08-07 23:07:51 -0500

    getting ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,24 @@ connectDB();
 // Initialize Middleware - no need for body parser with new version of express
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Dev stuff
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define the Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set the static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
